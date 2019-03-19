daab2e1c6ac96dc42d3c2fe529aab02618703277
Jessica Pope <jessica.pope.co@gmail.com>
2019-03-19 13:31:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-a = 1 
-b = 2
+a = 1, b = 2
 function add(a,b){return a + b}
 function subtract(a,b){return a - b}
 function multiply(a,b){return a*b}
-function divide(a,b){
-    return a/b
-    }
+function divide(a,b){return a/b}
